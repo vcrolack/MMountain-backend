a1c8cd53a9e41fde7870b40b925e787755bdecd7
Victor Rolack <victorolack@gmail.com>
2023-06-03 17:11:36 -0400

    add router api and refactory source code

--- routes/commonRoutes.js
+++ routes/commonRoutes.js
@@ -5,3 +5,5 @@ const commonController = require('../controllers/commonController');
 
 router.route('/validate-token')
   .post(commonController.validateToken)
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const usersRouter = require('./userRoutes');
+const commonRouter = require('./commonRoutes');
+const rootRouter = require('./root');
+
+const routerApi = (app) => {
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/users', usersRouter);
+  router.use('/common', commonRouter);
+  router.use('/', rootRouter);
+  router.use('/', express.static(path.join(__dirname, 'public')));
+
+  router.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+      res.sendFile(path.join(__dirname, '../views/404.html'));
+    } else if (req.accepts('json')) {
+      res.json({ message: '404 Not Found' });
+    } else {
+      res.type('txt').send('404 Not Found');
+    }
+  });
+}
+
+module.exports = routerApi;
