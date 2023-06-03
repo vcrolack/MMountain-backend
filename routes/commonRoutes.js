6b9041ebf3e36e11d03eeaae23a752c906c71af9
Victor Rolack <victorolack@gmail.com>
2023-06-03 13:11:18 -0400

    add commonRoutes and commonControllers

---
+++ routes/commonRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+const commonController = require('../controllers/commonController');
+
+router.route('/validate-token')
+  .post(commonController.validateToken)
