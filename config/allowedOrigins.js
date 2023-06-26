93c60788a59cfd5515a3ab401e33101109e27ed0
Victor Rolack <victorolack@gmail.com>
2023-06-25 22:57:15 -0400

    hotfix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [ // define una matriz de cadenas que representan los or√
     'http://localhost:3000', // permite solicitudes desde localhost en el puerto 3000
     'https://www.MMointain.cl', // permite solicitudes desde www.MMountain.cl con HTTPS
     'https://MMountain.cl', // permite solicitudes desde MMountain.cl con HTTPS
-    'https://mmountain-backend-production.up.railway.app/'
+    'https://mmountain-backend-production.up.railway.app'
 ]
 
 module.exports = allowedOrigins // exporta la matriz de or√≠genes permitidos para que se pueda utilizar en otros archivos
